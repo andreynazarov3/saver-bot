755236f802c0aae03c490781c64c33f7174b23c6
Andrey Nazarov <nazarov@dobroagency.ru>
2017-05-01 22:08:47 +0400

    commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello Heroku!');
+    res.send('Hello Github && Heroku!');
 });
 
 app.listen(process.env.PORT || 5000, function () {
