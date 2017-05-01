ed23362b4a13c07aa79a6308f7b0d43e90b912e8
Andrey Nazarov <nazarov@dobroagency.ru>
2017-05-01 23:19:10 +0400

    new

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const Telegraf = require('telegraf')
 const app = new Telegraf(process.env.BOT_TOKEN)
+const admin = require("firebase-admin");
+
+admin.initializeApp({
+    credential: admin.credential.cert({
+        projectId: process.env.PROJECT_ID,
+        clientEmail: process.env.CLIENT_EMAIL,
+        privateKey: process.env.PRIVATE_KEY
+    }),
+    databaseURL: process.env.DATABASE_URL
+})
+
+const db = admin.database();
+const ref = db.ref("/");
+ref.on("value", function(snapshot) {
+    console.log(snapshot.val());
+});
+
 
 // Set telegram webhook
-app.telegram.setWebhook(process.env.WEBHOOK_URL).then(function (result) {
-    console.log('set webhook success: ' + result)
-})
+app.telegram.setWebhook(process.env.WEBHOOK_URL)
+    .then(function (result) {
+        console.log('set webhook success: ' + result)
+    })
     .catch(function (err) {
         console.log('set webhook error: ' + err)
     })
@@ -17,4 +35,3 @@ app.on('message', (ctx) => {
 })
 
 app.startWebhook("/webhook", null, process.env.PORT || 5000)
-
