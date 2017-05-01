471a034a1997f6a98c2c851ee9c101e245759177
Andrey Nazarov <nazarov@dobroagency.ru>
2017-05-01 22:35:14 +0400

    new

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.telegram.setWebhook(process.env.WEBHOOK_URL).then(function (result) {
     .catch(function (err) {
         console.log('set webhook error: ' + err)
     })
-app.startWebhook(process.env.WEBHOOK_URL, null, process.env.port || 5000)
+app.startWebhook(process.env.WEBHOOK_URL, null, process.env.PORT || 5000)
 
 app.on('message', (ctx) => {
     ctx.reply('i listen to you!')
