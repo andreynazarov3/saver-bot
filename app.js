d249406b8ca1444f683f30b8d7b939321c89515d
Andrey Nazarov <nazarov@dobroagency.ru>
2017-05-05 00:59:26 +0400

    new

--- app.js
+++ app.js
@@ -1,32 +1,23 @@
 const Telegraf = require('telegraf')
+const https = require('https')
 const app = new Telegraf(process.env.BOT_TOKEN)
 const admin = require("firebase-admin")
 const serviceAccount = require("./saver-bot-firebase-adminsdk-8hra6-a7ca5b3db6.json")
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://saver-bot.firebaseio.com"
-})
-const FetchStream = require("fetch").FetchStream
 const fs = require("fs")
-
-out = fs.createWriteStream('file.html');
-new FetchStream("http://www.example.com/index.php").pipe(out);
+const gcloud = require('google-cloud');
 const db = admin.database()
 const posts = db.ref("/posts")
 const users = db.ref("/users")
 
-const gcloud = require('google-cloud');
-
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://saver-bot.firebaseio.com"
+})
 const storage = gcloud.storage({
     projectId: 'saver-bot',
     keyFilename: serviceAccount
 });
-
 const bucket = storage.bucket('saver-bot.appspot.com');
-
-posts.on("value", function (snapshot) {
-    console.log(snapshot.val())
-})
 
 
 // Set telegram webhook
@@ -42,39 +33,54 @@ app.on('message', (ctx) => {
     if (ctx.updateSubType === "text") {
         ctx.reply('saving this text...')
         users.child(ctx.message.from.id).push({
+            type: 'text',
             text: ctx.message.text,
-            data: ctx.message.date
+            createdAt: ctx.message.date
         })
             .then(() => {
-                ctx.reply('saved!')
+                return ctx.reply('saved!')
             })
             .catch((err) => {
-                ctx.reply('ooops, something went wrong ;(')
+                return ctx.reply('ooops, something went wrong ;(')
             })
     }
     else if (ctx.updateSubType === "photo") {
         let lastPhoto = ctx.message.photo.length
         let file_id = ctx.message.photo[lastPhoto - 1].file_id
-        console.log(file_id)
         app.telegram.getFileLink(file_id)
             .then((link) => {
-                console.log(link)
                 ctx.reply('saving photo...')
-                let file = fs.createWriteStream(file_id);
-                let fetch = new FetchStream(link).pipe(file);
-                fetch.on("end", function(){
-                    console.log('saved to disk');
+                return link
+            })
+            .then((link) => {
+                let fileName = file_id + ".jpg";
+                let file = fs.createWriteStream(fileName);
+                let request = https.get(link, function (response) {
+                    response.pipe(file)
+                })
+                file.on('finish', () => {
+                    console.error('All writes are now complete.');
                     // Upload a local file to a new file to be created in your bucket.
-                    bucket.upload(file, function(err, file) {
+                    bucket.upload(ctx.message.from.id + '/' + fileName, function(err, file) {
                         if (!err) {
-                            console.log('saved to google cloud')
-                            fs.unlinkSync(file);
-                        } else {
-                            console.log(err)
-                            fs.unlinkSync(file);
+                            fs.unlink(fileName,()=>console.log('file deleted'))
+                            console.log('upload finish');
+                            return fileName
                         }
                     });
                 });
+            })
+            .then((fileName)=> {
+                return users.child(ctx.message.from.id).push({
+                    type: 'photo',
+                    fileName: fileName,
+                    createdAt: ctx.message.date
+                })
+            })
+            .then(() => ctx.reply('file saved!'))
+            .catch((err) => {
+                console.log(err)
+                ctx.reply("oops, something went wrong, please try again :(")
             })
     } else {
         ctx.reply("sorry, i'm saving only text right now :(")
